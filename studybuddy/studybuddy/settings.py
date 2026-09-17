05942ed344593bb7d3b5d6aca90023fc2d64e531
DEV_BUDDY <allishittuabdulhameed@gmail.com>
2026-09-17 14:45:25 +0100

    resetup for vercel deployment

--- studybuddy/studybuddy/settings.py
+++ studybuddy/studybuddy/settings.py
@@ -142,8 +142,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
